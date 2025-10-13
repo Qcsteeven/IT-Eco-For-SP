3d4c2c10f2056fb05e568243c21392ccbe5b5c98
Lev Demonov <leva.demonov2005@gmail.com>
2025-10-13 13:55:47 +0800

    Initial commit

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,48 @@
+// app/api/chat/route.ts
+import { fireworks } from '@ai-sdk/fireworks';
+import {
+  streamText,
+  UIMessage,
+  convertToModelMessages,
+  tool,
+  stepCountIs,
+} from 'ai';
+import { z } from 'zod';
+
+
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages }: { messages: UIMessage[] } = await req.json();
+
+  const result = streamText({
+    model: fireworks('accounts/fireworks/models/gpt-oss-20b'),
+    messages: convertToModelMessages(messages),
+    stopWhen: stepCountIs(5),
+    // Эти tools НЕ будут вызваны deepseek-r1:
+    tools: {
+      weather: tool({
+        description: 'Get the weather in a location (fahrenheit)',
+        inputSchema: z.object({
+          location: z.string().describe('The location to get the weather for'),
+        }),
+        execute: async ({ location }) => {
+          const temperature = Math.round(Math.random() * (90 - 32) + 32);
+          return { location, temperature };
+        },
+      }),
+      convertFahrenheitToCelsius: tool({
+        description: 'Convert a temperature in fahrenheit to celsius',
+        inputSchema: z.object({
+          temperature: z.number().describe('The temperature in fahrenheit'),
+        }),
+        execute: async ({ temperature }) => {
+          const celsius = Math.round((temperature - 32) * (5 / 9));
+          return { celsius };
+        },
+      }),
+    },
+  });
+
+  return result.toUIMessageStreamResponse();
+}
